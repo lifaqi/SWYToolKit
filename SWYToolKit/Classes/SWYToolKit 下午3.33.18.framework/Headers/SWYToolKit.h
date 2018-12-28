12233cd4d3a6a722325f874103750b1fd34865f4
lifaqi <lfqswy@gmail.com>
2018-12-28 15:33:45 +0800

    no message

---
+++ SWYToolKit/Classes/SWYToolKit 下午3.33.18.framework/Headers/SWYToolKit.h
@@ -0,0 +1,20 @@
+//
+//  SWYToolKit.h
+//  SWYToolKit
+//
+//  Created by 淘翼购 on 2018/12/28.
+//  Copyright © 2018年 shenwuyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SWYToolKit.
+FOUNDATION_EXPORT double SWYToolKitVersionNumber;
+
+//! Project version string for SWYToolKit.
+FOUNDATION_EXPORT const unsigned char SWYToolKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SWYToolKit/PublicHeader.h>
+
+
+#import <SWYToolKit/ToolKit.h>
