abe4324ad0d3ae4dd9092451ff8d87de23a32155
lifaqi <lfqswy@gmail.com>
2018-12-29 15:52:02 +0800

    no message

--- Frameworks/SWYToolKit.framework/Headers/SWYToolKit.h
+++ Frameworks/SWYToolKit.framework/Headers/SWYToolKit.h
@@ -1,19 +1,35 @@
 //
-//  SWYToolKit.h
 //  SWYToolKit
 //
-//  Created by 淘翼购 on 2018/12/28.
+//  Created by 淘翼购 on 2018/10/17.
 //  Copyright © 2018年 shenwuyue. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for SWYToolKit.
-FOUNDATION_EXPORT double SWYToolKitVersionNumber;
+@interface SWYToolKit : NSObject
 
-//! Project version string for SWYToolKit.
-FOUNDATION_EXPORT const unsigned char SWYToolKitVersionString[];
+#pragma mark - view
+/// 判断UIView是否在滚动
++(BOOL)anySubViewScrolling:(UIView *)view;
 
-// In this header, you should import all the public headers of your framework using statements like #import <SWYToolKit/PublicHeader.h>
+#pragma mark - date
+/// 获取时间
++(NSDateComponents *)getDate:(NSDate *)date;
 
-#import <SWYToolKit/ToolKit.h>
+/// 获取当前日期,默认格式：yyyy-MM-dd HH:mm:ss
++(NSString *)getCurrentDate;
+
+/// 获取当前日期,自定义格式
++(NSString *)getCurrentDate:(NSString *)dateFormatter;
+
+/// 把日期字符串格式化为日期对象,默认格式：yyyy-MM-dd HH:mm:ss
++(NSDate *)getDateWithDateString:(NSString *)dateString;
+
+/// 把日期字符串格式化为日期对象，自定义格式
++(NSDate *)getDateWithDateString:(NSString *)dateString andDateFormatter:(NSString *)dateFormatter;
+
+/// 把日期对象格式化为日期字符串，自定义格式
++(NSString *)getDateStringWithDate:(NSDate *)date andDateFormatter:(NSString *)dateFormatter;
+
+@end
