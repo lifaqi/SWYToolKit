b787dde5842bdcdb748a5a85bff407be122fa522
lifaqi <lfqswy@gmail.com>
2018-12-29 09:53:22 +0800

    no message

--- Frameworks/SWYToolKit.framework/Headers/SWYToolKit.h
+++ Frameworks/SWYToolKit.framework/Headers/SWYToolKit.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char SWYToolKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SWYToolKit/PublicHeader.h>
 
-
+#import <ToolKit/ToolKit.h>
